af29058c0d90a18e56a16b4e8c2738d95028cbc2
Dmitry Tyurin <dmitry.tyurin@aspose.com>
2022-08-10 20:09:08 +0300

    WIP: created new MVC.Annotation project (9 build errors)

---
+++ Demos/GroupDocs.Annotation.AspNetMvc/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupDocs.Annotation.AspNetMvc.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // enable CORS
+            config.EnableCors();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
