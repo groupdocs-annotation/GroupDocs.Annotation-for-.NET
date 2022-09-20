4d0549c870338ffb905d443c4b5e4f11633a6d47
Dmitry Tyurin <dmitry.tyurin@aspose.com>
2022-09-20 14:15:30 +0300

    Removed legacy projects

--- Demos/WebForms/src/Products/Annotation/Entity/Request/TextCoordinatesRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using GroupDocs.Annotation.WebForms.Products.Common.Entity.Web;
-
-namespace GroupDocs.Annotation.WebForms.Products.Annotation.Entity.Request
-{
-    public class TextCoordinatesRequest : PostedDataEntity
-    {
-        public int pageNumber { get; set; }
-    }
-}
